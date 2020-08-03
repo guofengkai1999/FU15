717521313cfdf26562fb362d4901a180a2b8f4af
FU15 <69083642+guofengkai1999@users.noreply.github.com>
2020-08-03 12:01:17 +0900

    Add files via upload

---
+++ ex4.c
@@ -0,0 +1,46 @@
+//
+//  ex4.c
+//
+//
+//  Created by s1252003 on 2020/08/02.
+//
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int main(){
+    int coin, positive=0, negative=0;
+    char name[30];
+    srand(time(NULL));
+
+    //input mane
+    printf("Who are you?\n>");
+    scanf("%s",name);
+    printf("Hello, %s!\n",name);
+
+    //coin tos
+    printf("Tossing a coin...\n");
+
+    for(int i=0;i<3;i++){
+        printf("Round %d:",i);
+        coin=rand()%2;
+
+        if(coin==0){
+            positive++;
+            printf("Heads\n");
+        }
+        else{
+            negative++;
+            printf("Tails\n");
+        }
+    }
+
+    printf("Heads: %d, Tails: %d\n",positive,negative);
+
+    //judge
+    if(positive>negative) printf("%s won!\n",name);
+    else printf("%s lost.\n",name);
+  
+    return 0;
+}
